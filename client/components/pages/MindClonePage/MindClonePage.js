ec8bb83ddc9ba5495a1d15fbf1366648640085a2
Aditya Rana <official.adityarana01@gmail.com>
2022-01-19 20:39:45 +0530

    Added chat UI and message sending functionality.

--- client/components/pages/MindClonePage/MindClonePage.js
+++ client/components/pages/MindClonePage/MindClonePage.js
@@ -1,11 +1,54 @@
 import React from 'react';
 import { getResponse } from '_api/mindclone';
+import styles from '@chatscope/chat-ui-kit-styles/dist/default/styles.min.css';
+import {
+  MainContainer,
+  ChatContainer,
+  MessageList,
+  Message,
+  MessageInput,
+  ConversationHeader,
+  Avatar,
+} from '@chatscope/chat-ui-kit-react';
 
 export default function MindClonePage() {
+  const inputRef = React.useRef();
+  const [msgInputValue, setMsgInputValue] = React.useState('');
+  const [messages, setMessages] = React.useState([]);
+
+  const handleSend = message => {
+    setMessages([...messages, {
+      message,
+      direction: 'outgoing',
+    }]);
+    setMsgInputValue('');
+    inputRef.current.focus();
+  };
 
   return (
-    <div>
-      <h1>Mindclone</h1>
+    <div style={{ position: 'relative', height: '500px' }}>
+      <MainContainer>
+        <ChatContainer>
+          <ConversationHeader>
+            <Avatar
+              src="https://bot.to/wp-content/uploads/2020/10/elon-musk_5f7f8d1c1c775.png"
+              name="Elon"
+              status="available"
+            />
+            <ConversationHeader.Content userName="Elon Musk" info="Online" />
+          </ConversationHeader>
+          <MessageList>
+            {messages.map((m, i) => <Message key={i} model={m} />)}
+          </MessageList>
+          <MessageInput
+            placeholder="Type message here"
+            onSend={handleSend}
+            onChange={setMsgInputValue}
+            value={msgInputValue}
+            ref={inputRef}
+          />
+        </ChatContainer>
+      </MainContainer>
     </div>
   );
 }
