9c7ed28f820280ce9e61df32754de94bea2ffe78
Aditya Rana <official.adityarana01@gmail.com>
2022-02-01 21:06:35 +0530

    Navbar for mobile added.

--- client/components/organisms/Navigation/Navigation.js
+++ client/components/organisms/Navigation/Navigation.js
@@ -13,12 +13,14 @@ import UserDropdown from '_molecules/UserDropdown';
 
 import NavbarStartItems from './NavbarStartItems';
 import NavbarEndItems from './NavbarEndItems';
+import NavbarMobile from './NavbarMobile';
 
 export default function Navigation() {
   const { user } = useSelector(R.pick(['user']));
 
   const [auth, setAuth] = useState(!R.isEmpty(user));
   const [open, setOpen] = useState(false);
+  const [navbarMobileOpen, setNavbarMobileOpen] = useState(false);
 
   useEffect(() => {
     setAuth(!R.isEmpty(user));
@@ -29,93 +31,100 @@ export default function Navigation() {
   const closeDropdown = () => setOpen(false);
 
   return (
-    <Navbar fixed="top" shadow>
-      <Container>
-        <Navbar.Brand>
-          <Navbar.Item
-            to={auth ? '/home' : '/'}
-            aria-label="main navigation"
-            component={Link}
-          >
-            <Title className="logo" size="3">
-              AI Avatar
-            </Title>
-          </Navbar.Item>
-          <div className="navbar-brand-right">
-            {!auth && (
-              <Navbar.Item
-                className="is-hidden-desktop"
-                to="/login"
-                component={Link}
-              >
-                <Title size="6">
-                  Login
-                </Title>
-              </Navbar.Item>
-            )}
-            {!auth && (
-              <Navbar.Item
-                className="is-hidden-desktop"
-                to="/register"
-                component={Link}
-              >
-                <Button color="success">Sign Up</Button>
-              </Navbar.Item>
-            )}
-            {auth && (
-              <Navbar.Item
-                className="is-hidden-desktop"
-                onClick={toggleDropdown}
-                onKeyPress={toggleDropdown}
-                hoverable
-                component="a"
-              >
-                <Image size="32x32">
-                  <Image.Content
-                    className="profile-img"
-                    src={user.profilePic || '/images/default-profile.png'}
-                  />
-                </Image>
-                <span className="dropdown-caret" />
-              </Navbar.Item>
-            )}
-          </div>
-        </Navbar.Brand>
+    <div>
+      <Navbar fixed="top" shadow>
+        <Container>
+          <Navbar.Brand>
+            <Navbar.Item
+              to={auth ? '/home' : '/'}
+              aria-label="main navigation"
+              component={Link}
+            >
+              <Title className="logo" size="3">
+                AI Avatar
+              </Title>
+            </Navbar.Item>
+            <div className="navbar-brand-right">
+              {!auth && (
+                <Navbar.Item
+                  className="is-hidden-desktop"
+                  to="/login"
+                  component={Link}
+                >
+                  <Title size="6">Login</Title>
+                </Navbar.Item>
+              )}
+              {!auth && (
+                <Navbar.Item
+                  className="is-hidden-desktop"
+                  to="/register"
+                  component={Link}
+                >
+                  <Button color="success">Sign Up</Button>
+                </Navbar.Item>
+              )}
+              {auth && (
+                <Navbar.Item
+                  className="is-hidden-desktop"
+                  onClick={toggleDropdown}
+                  onKeyPress={toggleDropdown}
+                  hoverable
+                  component="a"
+                >
+                  <Image size="32x32">
+                    <Image.Content
+                      className="profile-img"
+                      src={user.profilePic || '/images/default-profile.png'}
+                    />
+                  </Image>
+                  <span className="dropdown-caret" />
+                </Navbar.Item>
+              )}
+            </div>
+            <Navbar.Burger
+              onClick={() => setNavbarMobileOpen(!navbarMobileOpen)}
+            />
+          </Navbar.Brand>
 
-        {auth ? (
-          <Navbar.Menu>
-            <NavbarStartItems />
-            <Navbar.End>
-              <NavbarEndItems />
-              <Navbar.Item onClick={toggleDropdown} onKeyPress={toggleDropdown} hoverable component="a">
-                <Image size="32x32">
-                  <Image.Content
-                    className="profile-img"
-                    src={user.profilePic || '/images/default-profile.png'}
-                  />
-                </Image>
-                <span className="dropdown-caret" />
-              </Navbar.Item>
-            </Navbar.End>
-          </Navbar.Menu>
-        ) : (
-          <Navbar.Menu>
-            <NavbarStartItems />
-            <Navbar.End>
-              <NavbarEndItems />
-              <Navbar.Item to="/login" component={Link}>
-                <Title size="6">
-                  Login
-                </Title>
-              </Navbar.Item>
-              <Navbar.Item to="/register" component={Link}>
-                <Button color="success">Sign Up</Button>
-              </Navbar.Item>
-            </Navbar.End>
-          </Navbar.Menu>
-        )}
-        <UserDropdown open={open} closeDropdown={closeDropdown} />
-      </Container>
-    </Navbar>
+          {auth ? (
+            <Navbar.Menu>
+              <NavbarStartItems />
+              <Navbar.End>
+                <NavbarEndItems />
+                <Navbar.Item
+                  onClick={toggleDropdown}
+                  onKeyPress={toggleDropdown}
+                  hoverable
+                  component="a"
+                >
+                  <Image size="32x32">
+                    <Image.Content
+                      className="profile-img"
+                      src={user.profilePic || '/images/default-profile.png'}
+                    />
+                  </Image>
+                  <span className="dropdown-caret" />
+                </Navbar.Item>
+              </Navbar.End>
+            </Navbar.Menu>
+          ) : (
+            <Navbar.Menu>
+              <NavbarStartItems />
+              <Navbar.End>
+                <NavbarEndItems />
+                <Navbar.Item to="/login" component={Link}>
+                  <Title size="6">Login</Title>
+                </Navbar.Item>
+                <Navbar.Item to="/register" component={Link}>
+                  <Button color="success">Sign Up</Button>
+                </Navbar.Item>
+              </Navbar.End>
+            </Navbar.Menu>
+          )}
+          <UserDropdown open={open} closeDropdown={closeDropdown} />
+        </Container>
+      </Navbar>
+      {navbarMobileOpen && <NavbarMobile />}
+    </div>
   );
 }
