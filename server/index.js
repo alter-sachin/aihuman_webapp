098ae4ea0952457a71b74cc40c519bfedb436c46
Aditya Rana <official.adityarana01@gmail.com>
2022-02-07 17:42:21 +0530

    Configs for deployment.

--- server/index.js
+++ server/index.js
@@ -20,4 +20,11 @@ configPassport(app, express);
 
 app.use('/', routes);
 
+if (process.env.NODE_ENV === 'production') {
+  app.use(expess.static('client/build'));
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
+
 app.listen(port, () => console.log(`Server is listening on port ${port}`));
