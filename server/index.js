7350ff8261304e843e90879d4b6e0fc49bdf473d
Aditya Rana <official.adityarana01@gmail.com>
2022-02-07 18:48:20 +0530

    Changed config.

--- server/index.js
+++ server/index.js
@@ -21,7 +21,7 @@ configPassport(app, express);
 app.use('/', routes);
 
 if (process.env.NODE_ENV === 'production') {
-  app.use(expess.static('client/build'));
+  app.use(express.static('client/build'));
   app.get('*', (req, res) => {
     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
   });
