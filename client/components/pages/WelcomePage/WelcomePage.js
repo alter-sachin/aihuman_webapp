da4fc298e025ee180c2390ba43cbe907db767047
Aditya Rana <official.adityarana01@gmail.com>
2022-03-16 19:06:54 +0530

    Homepage design

--- client/components/pages/WelcomePage/WelcomePage.js
+++ client/components/pages/WelcomePage/WelcomePage.js
@@ -1,27 +1,71 @@
-import React, { Suspense, useRef } from 'react';
-import { Canvas } from '@react-three/fiber';
-import { Environment } from '@react-three/drei';
-import Model from './Model';
-import Overlay from './Overlay';
+import React, { useState } from 'react';
+import './styles.css';
+import TinySlider from 'tiny-slider-react';
+
+const images = [
+  { id: 0, src: 'https://www.internetmadenft.com/assets/images/hero/personaje-01.webp' },
+  { id: 1, src: 'https://www.internetmadenft.com/assets/images/hero/personaje-02.webp' },
+  { id: 2, src: 'https://www.internetmadenft.com/assets/images/hero/personaje-03.webp' },
+  { id: 3, src: 'https://www.internetmadenft.com/assets/images/hero/personaje-04.webp' },
+];
 
 export default function WelcomePage() {
-  const overlay = useRef();
-  const caption = useRef();
-  const scroll = useRef(0);
+  const [heroImage, setHeroImage] = useState(images[3].src);
+
+  const changeSlide = (slideClicked, info, event) => {
+    setHeroImage(event.target.currentSrc);
+  };
+
+  const settings = {
+    controls: false,
+    loop: true,
+    items: 1,
+    slideBy: 'page',
+    nav: false,
+    speed: 400,
+    gutter: 10,
+    autoplayButtonOutput: false,
+    mouseDrag: true,
+    lazyload: true,
+    axis: 'vertical',
+    responsive: {
+      640: {
+        items: 2,
+      },
+
+      768: {
+        items: 3,
+      },
+    },
+  };
+
   return (
     <React.Fragment>
-      <Canvas
-        shadows
-        onCreated={(state) => state.events.connect(overlay.current)}
-        raycaster={{ computeOffsets: ({ clientX, clientY }) => ({ offsetX: clientX, offsetY: clientY }) }}
-      >
-        <ambientLight intensity={1} />
-        <Suspense fallback={null}>
-          <Model scroll={scroll} />
-          <Environment preset="city" />
-        </Suspense>
-      </Canvas>
-      <Overlay ref={overlay} caption={caption} scroll={scroll} />
+      <div className="hero-container">
+        <div className="hero-image">
+          <img src={heroImage} alt="3D Model" />
+        </div>
+
+        <div className="hero-slider-container">
+          <TinySlider
+            settings={settings}
+            className="hero-slider"
+            onClick={changeSlide}
+          >
+            {images.map((img) => (
+              <div
+                key={img.id}
+                className="hero-slider-card"
+              >
+                <img
+                  src={img.src}
+                  alt="3D Model"
+                />
+              </div>
+            ))}
+          </TinySlider>
+        </div>
+      </div>
     </React.Fragment>
   );
 }
